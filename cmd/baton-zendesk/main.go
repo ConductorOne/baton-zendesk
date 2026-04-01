27cc31df78866527cdb020e08c25422a4943be13
Luisina Santos <luisina.santos@conductorone.com>
2026-04-01 10:37:30 -0300

    refactor main function to use RunConnector

--- cmd/baton-zendesk/main.go
+++ cmd/baton-zendesk/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
 
 	configSdk "github.com/conductorone/baton-sdk/pkg/config"
+	"github.com/conductorone/baton-sdk/pkg/cli"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
-	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/conductorone/baton-zendesk/pkg/config"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"go.uber.org/zap"
-
 	"github.com/conductorone/baton-zendesk/pkg/connector"
 )
 
@@ -23,49 +18,21 @@ var (
 
 func main() {
 	ctx := context.Background()
-
-	_, cmd, err := configSdk.DefineConfiguration(
+	configSdk.RunConnector(
 		ctx,
 		connectorName,
-		getConnector,
+		version,
 		config.Config,
+		getConnector,
 		connectorrunner.WithDefaultCapabilitiesConnectorBuilderV2(&connector.Connector{}),
 		connectorrunner.WithSessionStoreEnabled(),
 	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	cmd.Version = version
-
-	err = cmd.Execute()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
 }
 
-func getConnector(ctx context.Context, cfg *config.Zendesk) (types.ConnectorServer, error) {
-	logger := ctxzap.Extract(ctx)
-
-	cb, err := connector.New(
-		ctx,
-		cfg.Orgs,
-		cfg.Subdomain,
-		cfg.Email,
-		cfg.ApiToken,
-	)
+func getConnector(ctx context.Context, cfg *config.Zendesk, _ *cli.ConnectorOpts) (connectorbuilder.ConnectorBuilderV2, []connectorbuilder.Opt, error) {
+	cb, err := connector.New(ctx, cfg.Orgs, cfg.Subdomain, cfg.Email, cfg.ApiToken)
 	if err != nil {
-		logger.Error("error creating connector", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
-
-	c, err := connectorbuilder.NewConnector(ctx, cb)
-	if err != nil {
-		logger.Error("error creating connector", zap.Error(err))
-		return nil, err
-	}
-
-	return c, nil
+	return cb, nil, nil
 }
